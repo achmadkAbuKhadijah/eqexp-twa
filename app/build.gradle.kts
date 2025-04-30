f99e96f7dbe7f93e752fdb06a542a913b0eba241
Achmad Kurnianto <achmad_kurnianto@yahoo.co.id>
2025-05-01 03:43:40 +0700

    refactor(config): :wrench: update compileSdk
    update `compileSdk` value from `build.gradle.kts` file into 36
    
    Signed-off-by: Achmad Kurnianto <achmad_kurnianto@yahoo.co.id>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,7 +141,7 @@ val twaManifest: TWAManifest = TWAManifest(
 )
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
